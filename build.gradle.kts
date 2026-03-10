bc4db9386433277f39448cd670cefac7774ccdb2
Dmitriy Zhavoronkov <dimaz.lark@gmail.com>
2026-03-11 00:11:56 +0100

    feat: add plugin verification for recommended IDEs in build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,12 @@ intellijPlatform {
         }
     }
 
+    pluginVerification {
+        ides {
+            recommended()
+        }
+    }
+
     signing {
         certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
         privateKey = providers.environmentVariable("PRIVATE_KEY")
